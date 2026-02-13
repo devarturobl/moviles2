ecf039710729e77ba154a10bdbb61591ad066171
devarturobl <4rthurbl00@gmail.com>
2026-02-13 13:32:17 -0600

    Parte2

--- semana2/xilophone/lib/main.dart
+++ semana2/xilophone/lib/main.dart
@@ -15,24 +15,45 @@ class XylophoneApp extends StatelessWidget {
         body: SafeArea(
           child: Center(
             child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
-                ElevatedButton(
-                  onPressed: (){
-                    final player = AudioPlayer();
-                    player.play(AssetSource('note1.wav'));
-                  }, 
-                  child: Text('Click me')),
-                  ElevatedButton(
-                  onPressed: (){
-                    final player = AudioPlayer();
-                    player.play(AssetSource('note2.wav'));
-                  }, 
-                  child: Text('Click me')),
+                botonS(color: Colors.red, sound: 'note1.wav'),
+                botonS(color: Colors.orange, sound: 'note2.wav'),
+                botonS(color: Colors.yellow, sound: 'note3.wav'),
+                botonS(color: Colors.green, sound: 'note4.wav'),
+                botonS(color: Colors.blue, sound: 'note5.wav'),
+                botonS(color: Colors.indigo, sound: 'note6.wav'),
+                botonS(color: Colors.purple, sound: 'note7.wav'),
               ],
             )
             ),
           ),
         ),
       );
+  }
+}
+
+
+class botonS extends StatelessWidget {
+  final Color color;
+  final String sound;
+  const botonS({required this.color, required this.sound, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: color,
+        foregroundColor: Colors.white,
+        minimumSize: const Size(200, 60),
+        elevation: 10,
+        shadowColor: color.withOpacity(0.5),
+      ),
+      onPressed: (){
+        final player = AudioPlayer();
+        player.play(AssetSource(sound));
+      }, 
+      child: Text('Click me')
+    );
   }
 }
